c158f9c216fad8f5178eb9d687226c6479113393
pordan92 <pordanl92@gmail.com>
2022-02-14 22:31:22 +0100

    error message design for Register window

--- autentication/src/components/SignIn.js
+++ autentication/src/components/SignIn.js
@@ -5,6 +5,26 @@ import classes from "./SignIn.module.css"
 import Input from "./Input";
 import buttonClasses from "./Login.module.css"
 
+const ErrorMessage = (props) => {
+    let message = ""
+    if(!props.validity && props.id === "name"){
+        message = "Chosen name is already taken!"
+    }
+    if(!props.validity && props.id === "mail"){
+        message = "Chosen e-mail is already taken!"
+    }
+    if(!props.validity && props.id === "pass"){
+        message = "Chosen password does not have at least 6 characters!"
+    }
+    if(!props.validity && props.id === "passConf"){
+        message = "Passwords are not identical!"
+    }
+
+    return <div className={classes.ErrorMessage}>
+        <p className={classes.message}>{message}</p>
+    </div>
+}
+
 const paramReducer = (state, action) => {
     if (action.type === "MAIL_&_VALIDATE") {
         return {
@@ -111,15 +131,18 @@ const SignIn = (props) => {
         }
     }
 
+
+
     return (
         <Modal onClose={props.onClose}>
             <form onSubmit={submitHandler}>
+                <ErrorMessage validity={loginParams.validName} id="name"/>
                 <Input ref={nameRef} input={{ type: "text", id: "name", placeholder: "Account name", changeEvent: true, validity: loginParams.validName }} onChange={nameHandler}>Name:</Input>
-
+                <ErrorMessage validity={loginParams.validMail} id="mail"/>
                 <Input ref={mailRef} input={{ type: "email", id: "mail", placeholder: "E-mail", changeEvent: true, validity: loginParams.validMail }} onChange={mailHandler}>E-mail:</Input>
-
+                <ErrorMessage validity={loginParams.validPass} id="pass"/>
                 <Input ref={passRef} input={{ type: "password", id: "pass", placeholder: "Password", changeEvent: true, validity: loginParams.validPass }} onChange={passwordHandler}>Password:</Input>
-
+                <ErrorMessage validity={loginParams.validConfPass} id="passConf"/>
                 <Input ref={confRef} input={{ type: "password", id: "passConf", placeholder: "Confirm Password", changeEvent: true, validity: loginParams.validConfPass }} onChange={passwordConfHandler}>Confirm Password:</Input>
                 
                 <div className={buttonClasses.buttonContainer}><button className={buttonClasses.button} type="submit">Register</button></div>
